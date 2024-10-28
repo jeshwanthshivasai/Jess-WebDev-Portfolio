b111740abe39260ae693b2861dc83c22a67fe98c
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-10-28 09:02:47 +0530

    rainbow highlight changes

--- components/Hero.js
+++ components/Hero.js
@@ -13,17 +13,17 @@ export default function Hero() {
         <RoughNotationGroup show={true}>
           <RainbowHighlight color={colors[0]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              ArchiTechie.
+              Architect.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Developer.
+              Designer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Designer.
+              Developer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
